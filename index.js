e8f6bc157300aa99323d1769c856b875e67c3c81
Rashmi S <110243712+RashmiSudheendra@users.noreply.github.com>
2023-06-30 14:23:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_PATH, () => {
 
 
 const PORT = process.env.SERVER_PORT || 9000
-const origin = process.env.CORS_ORIGIN || 'http://localhost:3000'
+const origin = process.env.CORS_ORIGIN || 'https://projectmanagementsite.netlify.app'
 
 const app = express()
 
